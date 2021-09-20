3831cb246ee5efe93ceb9b853daa82cf783f4e61
ZIKO Art 13 <lucian_avadani@yahoo.com>
2021-09-20 21:35:25 +0300

    20/09/ Edit JS Winows Open

--- suource.js
+++ suource.js
@@ -4,3 +4,13 @@ function link() {
 function link2() {
   window.location.assign("works.html") // Link pages//
 }
+function behanceburger(){
+  window.open("https://www.behance.net/gallery/123420367/website-Turnul-Burgerului", '_blank')
+}
+function gitburger(){
+  window.open("https://github.com/zikoart2013/TurnulBurgerului", '_blank')
+}
+
+function behancevilla(){
+  window.open("https://www.behance.net/gallery/124699693/laVilla-Bistro", '_blank')
+}
